289f1c1ede1192c6e5fe3e4d60678c9d2ce9e713
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-04-09 12:11:45 -0300

    IMC

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.meuapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +14,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editAltura, editPeso;
+    Button btnCalcular;
+    TextView tvResultado;
+    ImageView imageClassificacao;
+    double calculo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//        Button b = new Button(this);
-//        b.setText("asdf");
-//        setContentView(b);
+
+        editPeso=findViewById(R.id.editPeso);
+        editAltura=findViewById(R.id.editAltura);
+        tvResultado=findViewById(R.id.tvResultado);
+        imageClassificacao=findViewById(R.id.imageClassificacao);
+        btnCalcular=findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(v -> {
+            double peso=Double.parseDouble(editPeso.getText().toString());
+            double altura=Double.parseDouble(editAltura.getText().toString());
+
+            calculo= peso / (altura*altura);
+
+            tvResultado.setText(String.format("Seu IMC é %.2f", calculo));
+//            if (tvResultado < 18.5){
+//                imageClassificacao.setImageResource();
+//            }
+
+        });
+
+
+
 
     }
 }
