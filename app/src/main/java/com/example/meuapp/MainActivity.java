1a4d417b4d4c8b3cc846218a4e752ecbe2eb1808
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-04-15 12:08:20 -0300

    Mensagens entre activitys

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button b;
+    EditText ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
+
+        ed=findViewById(R.id.editTextmsg);
+
+        b=findViewById(R.id.btnMandar);
+        b.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(),ActivityB.class);
+            String msg= ed.getText().toString();
+            i.putExtra("msg",msg);
+            startActivity(i);
+
+        });
+
+        public void iniciaNovaActivity()
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
