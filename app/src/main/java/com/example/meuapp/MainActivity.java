d95e4960bc5eb7598dacf17830d39c86d87b626f
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-04-22 11:56:43 -0300

    IMC-Telas

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,17 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         editPeso=findViewById(R.id.editPeso);
         editAltura=findViewById(R.id.editAltura);
-        tvResultado=findViewById(R.id.tvResultado);
-        imageClassificacao=findViewById(R.id.imageClassificacao);
         btnCalcular=findViewById(R.id.btnCalcular);
 
         btnCalcular.setOnClickListener(v -> {
             double peso=Double.parseDouble(editPeso.getText().toString());
             double altura=Double.parseDouble(editAltura.getText().toString());
-
             calculo= peso / (altura*altura);
 
-            tvResultado.setText(String.format("Seu IMC é %.2f", calculo));
+            Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+            String msg= Double.toString(calculo);
+            i.putExtra("Calculo",msg);
+            startActivity(i);
+
+
+
+
+
+//            tvResultado.setText(String.format("Seu IMC é %.2f", calculo));
 //            if (tvResultado < 18.5){
 //                imageClassificacao.setImageResource();
 //            }
