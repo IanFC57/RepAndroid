7acc0a32c1028ff28d844fb1e914174b66484b2d
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-04-30 12:04:26 -0300

    Listagem

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    String nomes[] = new String[]{"Daniel", "Sofia", "Prancha", "Borracha", "Hugo", "Heitor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +21,31 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//        Button b = new Button(this);
-//        b.setText("asdf");
-//        setContentView(b);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
 
-    }
+    });
+        ListView listview = findViewById(R.id.listview);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+        );
+
+        listview.setAdapter(arrayAdapter);
+        listview.setOnItemClickListener((parent, view, position, id) -> {
+            Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+
+            i.putExtra("nome" , nomes[position]);
+            startActivity(i);
+        });
+
+}
+//parent, view, position, id ->{
+//            Intent i = new Intent(getApplicationContext().MainActivity2.class);
+//            i.putExtra("Nome", nome(position));
+//            startActivity(i);
 }
