1af9ed0ec28cf583b5022f876c569325bb37ab64
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-05-20 12:09:25 -0300

    SQLite

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +15,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    listView listView;
 
     SQLiteDatabase sqLiteDatabase;
 
@@ -38,13 +42,25 @@ public class MainActivity extends AppCompatActivity {
         //RECUPERAR DADOS
         Cursor c = sqLiteDatabase.rawQuery("SELECT id,id*10,titulo,nota FROM notas", null);
         c.moveToFirst();
+        ArrayList<Nota> listaNotas = new ArrayList<>();
         while (!c.isAfterLast()){
             @SuppressLint("Range") int id =c.getInt(c.getColumnIndex("id"));
             @SuppressLint("Range") String titulo=c.getString(c.getColumnIndex("titulo"));
             @SuppressLint("Range") String nota=c.getString(c.getColumnIndex("notas"));
+            Nota n = new Nota(id, titulo, nota);
+            listaNotas.add(n);
             Log.d("SELECT", Integer.toString(id)+","+titulo+","+nota);
             c.moveToNext();
         }
+
+        listView = findViewById(R.id.ListView);
+        ArrayList<String> listaTitulos = new ArrayList<>();
+        for (Nota nota :listaNotas){
+            listaTitulos.add(nota.titulo);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listaTitulos);
+        listView.setAdapter(adapter);
 
 
 
