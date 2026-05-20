7504413b200cff2464508e0eb0f60e2b5ae0eed8
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-05-20 12:09:14 -0300

    SQLite

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.meuapp;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -11,20 +16,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLiteDatabase sqLiteDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//        Button b = new Button(this);
-//        b.setText("asdf");
-//        setContentView(b);
+        sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT)");
+        // nunca fazer: String titulo=João;
+        //sqLiteDatabase.execSQL("INSERT INTO notas VALUES (1,''+titulo+ '','')");
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo","João");
+        contentValues.put("nota","777");
+        sqLiteDatabase.insert("notas", null, contentValues);
+
+        //RECUPERAR DADOS
+        Cursor c = sqLiteDatabase.rawQuery("SELECT id,id*10,titulo,nota FROM notas", null);
+        c.moveToFirst();
+        while (!c.isAfterLast()){
+            @SuppressLint("Range") int id =c.getInt(c.getColumnIndex("id"));
+            @SuppressLint("Range") String titulo=c.getString(c.getColumnIndex("titulo"));
+            @SuppressLint("Range") String nota=c.getString(c.getColumnIndex("notas"));
+            Log.d("SELECT", Integer.toString(id)+","+titulo+","+nota);
+            c.moveToNext();
+        }
+
+
+
+
 
     }
 }
