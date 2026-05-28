98c9292fee6c6da39c595bbe23468e4e997b80da
Ian <ian.c2007@aluno.ifsc.edu.br>
2026-05-28 11:43:44 -0300

    SQL

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -7,18 +7,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    listView listView;
+    ListView listView;
+    EditText editText;
+    ImageButton imageButton;
 
     SQLiteDatabase sqLiteDatabase;
 
@@ -29,15 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getString(R.string.app_name);
         sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        imageButton = findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(v -> {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("titulo","João");
+            contentValues.put("nota","777");
+            sqLiteDatabase.insert("notas", null, contentValues);
+        });
 
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT)");
         // nunca fazer: String titulo=João;
         //sqLiteDatabase.execSQL("INSERT INTO notas VALUES (1,''+titulo+ '','')");
 
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("titulo","João");
-        contentValues.put("nota","777");
-        sqLiteDatabase.insert("notas", null, contentValues);
+
 
         //RECUPERAR DADOS
         Cursor c = sqLiteDatabase.rawQuery("SELECT id,id*10,titulo,nota FROM notas", null);
@@ -55,16 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.ListView);
         ArrayList<String> listaTitulos = new ArrayList<>();
-        for (Nota nota :listaNotas){
+        for (Nota nota: listaNotas){
             listaTitulos.add(nota.titulo);
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listaTitulos);
         listView.setAdapter(adapter);
-
-
-
-
-
     }
 }
